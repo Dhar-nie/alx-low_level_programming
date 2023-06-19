6f94d370946d4a19c23ab08e9770464e7ae5b7ad
Dhar-nie <nmajudaniel96@gmail.com>
2023-06-19 09:13:18 -0700

    Grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,10 @@ int main(void)
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(S)\n", sizeof(int));
 	printf("Size of a long: %d byte(s)\n", sizeof(long));
-	printf("Size of a long long: %d byte(s)\n", sizeof(long long));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
-	return (0);
+	printf("Size of a short: %d byte(s)\n", sizeof(short));
+	printf("Size of a long long: %d byte(s)", sizeof(long long));
+	printf("Size of a float: %d byte(s)", sizeof(float));
+	printf("Size of double: %d byte(s)", sizeof(double));
+	printf("Size of long double: %d byte(s)", sizeof(long double));
+	return 0;
 }
